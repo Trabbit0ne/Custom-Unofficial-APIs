cc69c45b9f0dfc4bae6872904f8a2958dd6f8764
Trabbit0ne <trabbit0neyt@gmail.com>
2025-01-05 17:51:08 -0500

    Update neocities.sh

--- neocities.sh
+++ neocities.sh
@@ -13,13 +13,23 @@ tag="$1"
 echo -e "\e[32mExtracting URLs for the tag: $tag...\e[0m"
 
 # Fetch URLs, filter and exclude unwanted URLs
-curl -s "https://neocities.org/browse?tag=$tag" | \
-grep -oP 'href="([^"]+)' | \
-grep -oP 'httpss?://\S+' | \
+urls=$(curl -s "https://neocities.org/browse?tag=$tag" | \
+grep -oP 'href="https?://[^"]+' | \
+grep -oP 'https?://\S+' | \
 grep -v -e 'https://duckduckgo.com/' \
          -e 'https://neocities.org' \
          -e 'https://github.com/neocities' \
-         -e 'https://bsky.app/profile/neocities.org'
+         -e 'https://bsky.app/profile/neocities.org' | \
+sort | uniq)
+
+# Check if any URLs were found
+if [ -z "$urls" ]; then
+  echo -e "\e[31mNo URLs found for the tag: $tag.\e[0m"
+  exit 1
+fi
+
+# Display the URLs
+echo "$urls"
 
 # Inform the user when the extraction is complete
 echo -e "\e[32mURL extraction complete.\e[0m"
